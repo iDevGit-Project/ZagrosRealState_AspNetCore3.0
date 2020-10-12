82562be578bbe550012f9ca7118a51d9adbb2d4d
Hamed Shariaty Mehr <nextlab.info@gmail.com>
2020-10-12 15:35:56 +0330

    Init project

--- ZagrosRealState/obj/Debug/netcoreapp3.0/ZagrosRealState.AssemblyInfo.cs
+++ ZagrosRealState/obj/Debug/netcoreapp3.0/ZagrosRealState.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("f994e2e3-535d-4549-b5ef-1457977151a0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZagrosRealState")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
